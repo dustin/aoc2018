c4d8dd1b86043a9dadc4c1e9928d722b3498a8a7
Dustin Sallings <dustin@spy.net>
2018-12-01 21:59:29 -0800

    Finished day 2

---
+++ src/Day2.hs
@@ -0,0 +1,34 @@
+module Day2 where
+
+import Control.Monad (guard)
+import Data.List (sortOn)
+import Text.EditDistance (levenshteinDistance, defaultEditCosts)
+import qualified Data.Set as Set
+import qualified Data.Map.Strict as Map
+
+checksum :: [String] -> Int
+checksum lns =
+  -- this was kind of gross, but it was the first thing I could think of.
+  let w = (Map.fromListWith (+) <$> map (\x -> (x,1)) <$> lns) :: [Map.Map Char Int]
+      (t,f) = foldr (\m (t,f) -> (t + if null $ Map.filter (==2) m then 0 else 1,
+                                  f + if null $ Map.filter (==3) m then 0 else 1)) (0,0) w in
+    t*f
+
+part1 :: IO ()
+part1 = print =<< checksum <$> lines <$> readFile "input/day2"
+
+match :: [String] -> [String]
+match xs = do
+  l1 <- xs
+  l2 <- xs
+
+  -- There's really a special case that can apply here since the
+  -- distance we're looking for is one as a single character
+  -- replacement.  Could've just done a
+  -- length . filter (uncurry (/=)) $ zip l1 l2
+  guard $ levenshteinDistance defaultEditCosts l1 l2 == 1
+
+  pure $ map fst $ filter (uncurry (==)) $ zip l1 l2
+
+part2 :: IO ()
+part2 = print =<< head . match <$> lines <$> readFile "input/day2"
