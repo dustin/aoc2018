79553ba5df754659679d839479f6f78190647737
Dustin Sallings <dustin@spy.net>
2018-12-10 13:20:11 -0800

    Fixed prop_min to not pass minimum

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,19 +10,19 @@ import Day10 (findMin)
 testFindMin :: [TestTree]
 testFindMin = map (\(f, xs, want) -> testCase (show xs) $ assertEqual "" want (findMin f xs)) [
   (id, [9], 9),
-  (id, [9, 7, 5, 4, 3, 2, 3], 2)
+  (id, [9, 7, 5, 4, 3, 2, 3, 1], 2)
   ]
 
 -- Every value before the min value should be greater than the min value.
-prop_findMin :: (NonEmptyList Int) -> Bool
-prop_findMin (NonEmpty xs) = let n = findMin id xs in
-                               all (> n) . takeWhile (/= n) $ xs
+prop_min :: (NonEmptyList Int) -> Bool
+prop_min f (NonEmpty xs) = let n = findMin id $ xs in
+                              and . (\l -> zipWith (>=) l $ tail l) . takeWhile (/= n) $ xs
 
 tests :: [TestTree]
 tests = [
   testGroup "findMin" testFindMin,
 
-  testProperty "findMin" prop_findMin
+  localOption (QC.QuickCheckTests 10000) $ testProperty "findMin" prop_min
   ]
 
 main :: IO ()
