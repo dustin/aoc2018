c791bfaaaff345ff8d5faa1796850ecb0ca2c746
Dustin Sallings <dustin@spy.net>
2018-12-10 10:57:21 -0800

    Less dumb day 10

--- src/Day10.hs
+++ src/Day10.hs
@@ -14,7 +14,7 @@ import Data.List.Extra (chunksOf)
 
 -- position=< 9,  1> velocity=< 0,  2>
 
-data Vec = Vec (Int,Int) (Int,Int) deriving (Show)
+data Vec = Vec (Int,Int) (Int,Int) deriving (Eq, Ord, Show)
 
 parseVec :: A.Parser Vec
 parseVec = do
@@ -31,8 +31,17 @@ parseVec = do
 getInput :: IO (Either String [Vec])
 getInput = A.parseOnly (parseVec `A.sepBy` (A.char '\n'))<$> pack <$> readFile "input/day10"
 
+-- Move a vector to where it should be after n seconds
 vecMove :: Int -> Vec -> Vec
 vecMove by (Vec (x,y) v@(vx,vy)) = Vec (x + (vx * by), y + (vy*by)) v
+
+-- Translate a vector by a given x and y value
+vecTrans :: (Int,Int) -> Vec -> Vec
+vecTrans (x,y) (Vec (px,py) v) = Vec (px+x,py+y) v
+
+-- Move a list of vectors so that the min bound is origin
+originate :: [Vec] -> [Vec]
+originate vs = let ((minx,miny),_) = bounds vs in vecTrans (-minx, -miny) <$> vs
 
 bounds :: [Vec] -> ((Int,Int),(Int,Int))
 bounds vs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
@@ -43,18 +52,23 @@ bounds vs = ((minimum xs, minimum ys), (maximum xs, maximum ys))
     ys = map (\(Vec (_,y) _) -> y) vs
 
 drawVecs :: [Vec] -> IO ()
-drawVecs vs = let bs@((minx,miny), (maxx,maxy)) = bounds vs
+drawVecs vs = let vs' = originate vs
+                  bs@((minx,miny), (maxx,maxy)) = bounds vs'
                   m = Map.fromList $ [((x,y),' ') | x <- [0..maxx], y <- [0..maxy]]
-                  mm = chunksOf (maxx+1) $ map snd $ sortBy (comparing (snd . fst)) $ Map.toList $ Map.union (Map.fromList $ map (\(Vec p _) -> (p,'#')) vs) m in
+                  mm = chunksOf (maxx+1) $ map snd $ sortBy (comparing (snd . fst)) $ Map.toList $ Map.union (Map.fromList $ map (\(Vec p _) -> (p,'#')) vs') m in
                 mapM_ putStrLn mm
+
+findMin :: Ord b => (a -> b) -> [a] -> a
+findMin f (x:xs) = go xs x
+  where go [] r = r
+        go (x':xs') r
+          | f x' > f r = r
+          | otherwise = go xs' x'
 
 part1 :: IO ()
 part1 = do
   (Right vs) <- getInput
-  let candidates = filter (\x -> let ((minx,maxx),(miny,maxy)) = bounds $ vecMove x <$> vs in
-                                minx >= 0 && miny >= 0) [0..100000]
-  mapM_ (\n -> do
-            print n
-            let vs' = vecMove n <$> vs
-            print $ bounds $ vs'
-            drawVecs vs') candidates
+  let n = findMin (\n -> let vs' = originate (vecMove n <$> vs)
+                             (_,(w,h)) = bounds vs' in w*h) [1..100000]
+  print $ n
+  drawVecs $ vecMove n <$> vs
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,27 @@
 
 import Test.QuickCheck
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
+
+import Day10 (findMin)
+
+testFindMin :: [TestTree]
+testFindMin = map (\(f, xs, want) -> testCase (show xs) $ assertEqual "" want (findMin f xs)) [
+  (id, [9], 9),
+  (id, [9, 7, 5, 4, 3, 2, 3], 2)
+  ]
+
+-- Every value before the min value should be greater than the min value.
+prop_findMin :: (NonEmptyList Int) -> Bool
+prop_findMin (NonEmpty xs) = let n = findMin id xs in
+                               all (> n) . takeWhile (/= n) $ xs
 
 tests :: [TestTree]
 tests = [
+  testGroup "findMin" testFindMin,
+
+  testProperty "findMin" prop_findMin
   ]
 
 main :: IO ()
