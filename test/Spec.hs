225f43faa1db1b9a06ecc63e42368ffaf0499e2d
Dustin Sallings <dustin@spy.net>
2018-12-10 16:20:28 -0800

    Verify findMin ≠ minimum

--- test/Spec.hs
+++ test/Spec.hs
@@ -15,14 +15,22 @@ testFindMin = map (\(f, xs, want) -> testCase (show xs) $ assertEqual "" want (f
 
 -- Every value before the min value should be greater than the min value.
 prop_min :: (NonEmptyList Int) -> Bool
-prop_min f (NonEmpty xs) = let n = findMin id $ xs in
-                              and . (\l -> zipWith (>=) l $ tail l) . takeWhile (/= n) $ xs
+prop_min (NonEmpty xs) = let n = findMin id $ xs in
+                           and . (\l -> zipWith (>=) l $ tail l) . takeWhile (/= n) $ xs
+
+-- Verify findMin ≠ minimum by ensuring there's a small uptick and
+-- then still yet lower value at the end of the list.
+prop_findMinNotMin :: (NonEmptyList Int) -> Bool
+prop_findMinNotMin (NonEmpty xs) = let mn = minimum xs
+                                       l = xs <> [succ mn, (pred.pred) mn] in
+                                     findMin id l /= minimum l
 
 tests :: [TestTree]
 tests = [
   testGroup "findMin" testFindMin,
 
-  localOption (QC.QuickCheckTests 10000) $ testProperty "findMin" prop_min
+  localOption (QC.QuickCheckTests 10000) $ testProperty "findMin" prop_min,
+  testProperty "findMin is not minimum" prop_findMinNotMin
   ]
 
 main :: IO ()
