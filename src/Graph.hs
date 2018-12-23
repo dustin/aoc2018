2f2e0baac2481ae3270e59c2d33acd0dad6f5d07
Dustin Sallings <dustin@spy.net>
2018-12-23 15:31:22 -0800

    Breaking graph traversal apart a bit more.

--- src/Graph.hs
+++ src/Graph.hs
@@ -7,7 +7,7 @@ import qualified Data.Set        as Set
 
 -- Tests for this are in Day22.
 
--- This gives the costs to points and the link maps
+-- This gives the costs to points and the link maps.
 dijkstra' :: Ord v => (v -> [(Int,v)]) -> v -> (v -> Bool) -> (Map v Int, Map v v)
 dijkstra' neighbrf start done = go (Q.singleton (0,start)) (Map.singleton start 0) mempty mempty
   where
@@ -24,15 +24,21 @@ dijkstra' neighbrf start done = go (Q.singleton (0,start)) (Map.singleton start 
         l' = Map.union (Map.fromList $ map (\(_,p') -> (p',pt)) moves) l
         psd = Q.fromList $ fmap (\(c,x) -> (d+c,x)) moves
 
+-- Using maps computed by dijkstra', find the cost and path from the
+-- start to a destination.
+resolvePath :: Ord v => Map v Int -> Map v v -> v -> v -> Maybe (Int,[v])
+resolvePath m l start end = case Map.lookup end m of
+                              Nothing   -> Nothing
+                              Just cost -> Just (cost, reverse $ end : go end)
+  where
+    go pt
+      | pt == start = []
+      | otherwise = next : go next
+      where next = l Map.! pt
+
+-- Given a function that returns a list of neighbors and their costs
+-- (Int), a starting point, and an ending point, return a the cost and
+-- path to get from the starting point to the ending point.
 dijkstra :: Ord v => (v -> [(Int,v)]) -> v -> v -> Maybe (Int,[v])
 dijkstra neighbrf start end = resolve (dijkstra' neighbrf start (== end))
-
-  where
-    resolve (m,l) = case Map.lookup end m of
-                      Nothing   -> Nothing
-                      Just cost -> Just (cost, reverse $ end : go end)
-      where
-        go pt
-          | pt == start = []
-          | otherwise = next : go next
-          where next = l Map.! pt
+  where resolve (m,l) = resolvePath m l start end
