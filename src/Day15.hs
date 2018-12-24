f34c22a152406c48cd3fc0080ef20f06a0a3a062
Dustin Sallings <dustin@spy.net>
2018-12-23 16:26:39 -0800

    Some day 15 improvements
    benchmarking day15/elfbattle/part2
    time                 1.802 ms   (1.781 ms .. 1.823 ms)
                         0.998 R²   (0.997 R² .. 0.999 R²)
    mean                 1.807 ms   (1.789 ms .. 1.837 ms)
    std dev              70.34 μs   (48.84 μs .. 107.6 μs)
    variance introduced by outliers: 26% (moderately inflated)

--- src/Day15.hs
+++ src/Day15.hs
@@ -1,47 +1,47 @@
-{-# LANGUAGE OverloadedStrings, TupleSections #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Day15 where
 
-import Debug.Trace (trace)
-import qualified Data.Map.Strict     as Map
-import qualified Data.Set as Set
-import Data.List (sortOn, sortBy, intercalate, foldl')
-import Data.Ord (comparing)
-import Control.Parallel.Strategies (parMap, rdeepseq)
-import Control.Applicative ((<|>))
-import Data.Tuple (swap)
-import Data.Function (on)
-import Data.Foldable (minimumBy)
-import Data.Maybe (listToMaybe, catMaybes)
-
+import           Control.Parallel.Strategies (parMap, rdeepseq)
+import           Data.Foldable               (minimumBy)
+import           Data.List                   (foldl', intercalate, sortBy,
+                                              sortOn)
+import qualified Data.Map.Strict             as Map
+import           Data.Maybe                  (listToMaybe)
+import           Data.Ord                    (comparing)
+import qualified Data.PQueue.Min             as Q
+import qualified Data.Set                    as Set
+import           Data.Tuple                  (swap)
+import           Debug.Trace                 (trace)
 
 data Thing = Wall | Open | Elf Int | Goblin Int
 
 instance Show Thing where
-  show Wall = "#"
-  show Open = "."
-  show (Elf x) = "E(" <> show x <> ")"
+  show Wall       = "#"
+  show Open       = "."
+  show (Elf x)    = "E(" <> show x <> ")"
   show (Goblin x) = "G(" <> show x <> ")"
 
 isOpen :: Thing -> Bool
 isOpen Open = True
-isOpen _ = False
+isOpen _    = False
 
 isElf :: Thing -> Bool
 isElf (Elf _) = True
-isElf _ = False
+isElf _       = False
 
 isGoblin :: Thing -> Bool
 isGoblin (Goblin _) = True
-isGoblin _ = False
+isGoblin _          = False
 
 isEnemy :: Thing -> Thing -> Bool
 isEnemy a b = (isElf a && isGoblin b) || (isGoblin a && isElf b)
 
 hp :: Thing -> Int
-hp (Elf x) = x
+hp (Elf x)    = x
 hp (Goblin x) = x
-hp x = error ("no HP for " <> show x)
+hp x          = error ("no HP for " <> show x)
 
 type HitFun = Thing -> Thing
 
@@ -78,7 +78,7 @@ parseInput lns = World $ Map.fromList $ concatMap (\(y,r) -> map (\(x,c) -> ((x,
         p 'E' = Elf 200
         p 'G' = Goblin 200
         p '.' = Open
-        p _ = Wall
+        p _   = Wall
 
 readingOrder :: (Int,Int) -> (Int,Int) -> Ordering
 readingOrder = comparing swap
@@ -102,7 +102,7 @@ around :: (Int,Int) -> [(Int,Int)]
 around (x,y) = [(x,y-1), (x-1,y), (x+1,y), (x,y+1)]
 
 at :: World -> (Int,Int) -> Thing
-at w@(World m) p = m Map.! p
+at (World m) p = m Map.! p
 
 mdist :: (Int,Int) -> (Int,Int) -> Int
 mdist (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
@@ -110,7 +110,6 @@ mdist (x1,y1) (x2,y2) = abs (x1-x2) + abs (y1-y2)
 targets :: World -> (Int,Int) -> [(Int,Int)]
 -- targets _ (23,8) = [(24,10)]
 targets w@(World m) pos = let enemies = ofType w (isEnemy (m Map.! pos))
-                              open = openSpace w
                               tset = openSpace w `Set.intersection` Set.fromList (concatMap around enemies) in
                             sortOn (mdist pos) (Set.toList tset)
 
@@ -144,14 +143,14 @@ pathTo :: World -> (Int,Int) -> (Int,Int) -> Maybe [(Int,Int)]
 pathTo w f t
   | f `adjacentTo` t = Just [t]
   | otherwise = -- trace (" finding path to " <> show t) $
-                let m = go [(t,0)] mempty mempty in
+                let m = go (Q.singleton (0,t)) mempty mempty in
                   if null m then Nothing
                   else sequenceA $ resolve f m
 
   where
     p1 `adjacentTo` p2 = p1 `elem` around p2
     ospac = openSpace w
-    possible p = Set.toList $ Set.fromList (around p) `Set.intersection` ospac
+    possible p = filter (`Set.member` ospac) (around p)
 
     resolve :: (Int,Int) -> Map.Map (Int,Int) Int -> [Maybe (Int,Int)]
     resolve p m
@@ -168,22 +167,24 @@ pathTo w f t
                                         t2 = m Map.! b in
                                       compare t1 t2 <> readingOrder a b)
 
-    go :: [((Int,Int),Int)] -> Map.Map (Int,Int) Int -> Set.Set (Int,Int) -> Map.Map (Int,Int) Int
-    go [] m _ = m
-    go ((p,d):odo) m seen
+    go :: Q.MinQueue (Int,(Int,Int)) -> Map.Map (Int,Int) Int -> Set.Set (Int,Int) -> Map.Map (Int,Int) Int
+    go q m seen
+      | Q.null q = m
       | f `elem` around p = go odo (Map.insert p d m) seen
       | null ps = go odo m seen
-      | otherwise = go (odo <> psd) (Map.insertWith min p d m) (Set.union seen (Set.fromList ps))
+      | otherwise = go (odo `Q.union` psd) (Map.insertWith min p d m) (Set.union seen (Set.fromList moves))
 
       where
+        ([(d,p)], odo) = Q.splitAt 1 q
+        moves = filter (\p' -> 1+d < Map.findWithDefault (1+d+1) p' m) ps
         ps = filter (`Set.notMember` seen) $ possible p
-        psd = map (,d+1) ps
+        psd = Q.fromList $ map (d+1,) moves
 
 adjacentEnemies :: World -> (Int,Int) -> [(Int,Int)]
 adjacentEnemies w p = filter (\x -> isEnemy (at w p) (at w x)) $ around p
 
 attack :: World -> (Int,Int) -> HitFun -> World
-attack w@(World m) p hit = World $ Map.adjust hit p m
+attack (World m) p hit = World $ Map.adjust hit p m
 
 move :: World -> (Int,Int) -> Maybe (Int,Int) -> World
 move w _ Nothing = w
@@ -193,13 +194,13 @@ move w@(World m) p (Just x)
 
 action :: World -> (Int,Int) -> HitFun -> World
 action w p hit = let (w', p') = case bestMove w p of
-                                  Nothing -> (w,p)
+                                  Nothing     -> (w,p)
                                   np@(Just x) -> (move w p np, x) in
                    attackOne w' p' $ adjacentEnemies w' p'
   where
     attackOne :: World -> (Int,Int) -> [(Int,Int)] -> World
     attackOne w' _ [] = w'
-    attackOne w'@(World m) p' ae = attack w' (best ae) hit
+    attackOne w'@(World m) _ ae = attack w' (best ae) hit
       where
         best :: [(Int,Int)] -> (Int,Int)
         best = minimumBy (\a b -> let a' = m Map.! a
@@ -207,11 +208,11 @@ action w p hit = let (w', p') = case bestMove w p of
                                     comparing hp a' b' <> readingOrder a b)
 
 gameOver :: World -> Bool
-gameOver w@(World m) = let (e,g) = foldr (\x o@(e,g) ->
-                                            case () of _
-                                                         | isElf x -> (e+1,g)
-                                                         | isGoblin x -> (e,g+1)
-                                                         | otherwise -> o)
+gameOver (World m) = let (e,g) = foldr (\x o@(e',g') ->
+                                           case () of _
+                                                        | isElf x -> (e'+1,g')
+                                                        | isGoblin x -> (e',g'+1)
+                                                        | otherwise -> o)
                                    (0,0) m in
                          e == 0 || g == 0
 
@@ -233,8 +234,6 @@ play' w i hit f = let (did,w') = aRound (f w i) hit in
 
 play :: World -> Int -> HitFun -> (Int, World)
 play w i hit = play' w i hit const
-  where
-    ms w i =  trace ("round " <> show i <> "\n" <> show w) w
 
 score :: World -> Int
 score w = sum (map (hp . at w) $ players w)
@@ -290,8 +289,8 @@ part2 = do
 
       where
         hit = mkHit n
-        go w = let (did,w') = aRound w hit in
-                 case () of _
-                              | not did -> (True,w')
-                              | length (elves w') < es -> (False,w')
-                              | otherwise -> go w'
+        go w'' = let (did,w') = aRound w'' hit in
+                   case () of _
+                                | not did -> (True,w')
+                                | length (elves w') < es -> (False,w')
+                                | otherwise -> go w'
