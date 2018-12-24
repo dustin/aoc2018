e5cf290785ca63bd9c5225320fd8ac54c3392e70
Dustin Sallings <dustin@spy.net>
2018-12-24 12:42:49 -0800

    Trying out some haddock

--- src/Search.hs
+++ src/Search.hs
@@ -1,4 +1,15 @@
-module Search where
+{-|
+Module      : Search
+Description : Searching stuff for AoC.
+Copyright   : (c) Dustin Sallings, 2018
+License     : MIT
+Maintainer  : dustin@spy.net
+Stability   : experimental
+Portability : POSIX
+
+Things i use for searching space in AoC.
+-}
+module Search (dijkstra', dijkstra, resolveDijkstra, binSearch) where
 
 import           Data.Map        (Map)
 import qualified Data.Map.Strict as Map
@@ -7,8 +18,17 @@ import qualified Data.Set        as Set
 
 -- Tests for this are in Day22.
 
--- This gives the costs to points and the link maps.
-dijkstra' :: Ord v => (v -> [(Int,v)]) -> v -> (v -> Bool) -> (Map v Int, Map v v)
+-- | 'dijkstra'' uses [Dijkstra's
+-- Algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) to
+-- find the costs and links from a starting point to various points on
+-- a graph.
+--
+-- See 'resolveDijkstra' for a means of determining a path from the
+-- resulting values.
+dijkstra' :: Ord v => (v -> [(Int,v)]) -- ^ Provide a list of all neighbors of v with their associated costs.
+  -> v -- ^ The starting point.
+  -> (v -> Bool) -- ^ Predicate allowing early termination of search (if True).
+  -> (Map v Int, Map v v) -- ^ (cost from origin to v, links from points to points)
 dijkstra' neighbrf start done = go (Q.singleton (0,start)) (Map.singleton start 0) mempty mempty
   where
     go q m l seen
@@ -24,27 +44,31 @@ dijkstra' neighbrf start done = go (Q.singleton (0,start)) (Map.singleton start 
         l' = Map.union (Map.fromList $ map (\(_,p') -> (p',pt)) moves) l
         psd = Q.fromList $ fmap (\(c,x) -> (d+c,x)) moves
 
--- Using maps computed by dijkstra', find the cost and path from the
+-- | Using maps computed by 'dijkstra'', find the cost and path from the
 -- start to a destination.
-resolvePath :: Ord v => Map v Int -> Map v v -> v -> v -> Maybe (Int,[v])
-resolvePath m l start end = case Map.lookup end m of
-                              Nothing   -> Nothing
-                              Just cost -> Just (cost, reverse $ end : go end)
+resolveDijkstra :: Ord v => Map v Int -> Map v v -> v -> v -> Maybe (Int,[v])
+resolveDijkstra m l start end = case Map.lookup end m of
+                                  Nothing   -> Nothing
+                                  Just cost -> Just (cost, reverse $ end : go end)
   where
     go pt
       | pt == start = []
       | otherwise = next : go next
       where next = l Map.! pt
 
--- Given a function that returns a list of neighbors and their costs
--- (Int), a starting point, and an ending point, return a the cost and
--- path to get from the starting point to the ending point.
-dijkstra :: Ord v => (v -> [(Int,v)]) -> v -> v -> Maybe (Int,[v])
+-- | 'dijkstra' uses [Dijkstra's
+-- Algorithm](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm) to
+-- find the lowest cost path to a given destination.
+dijkstra :: Ord v => (v -> [(Int,v)]) -- ^ Provide a list of all neighbors of v with their associated costs.
+         -> v -- ^ The starting point.
+         -> v -- ^ The destination point.
+         -> Maybe (Int,[v])  -- ^ The cost to the destination, and the path to get there.
 dijkstra neighbrf start end = resolve (dijkstra' neighbrf start (== end))
-  where resolve (m,l) = resolvePath m l start end
+  where resolve (m,l) = resolveDijkstra m l start end
 
--- Search across a number space to find a boundary.
-binSearch :: Integral a => (a -> Ordering) ->  a -> a -> a
+-- | 'binSearch' performs a binary search to find the boundary
+-- function where a function returns its highest 'LT' value.
+binSearch :: Integral a => (a -> Ordering) -> a -> a -> a
 binSearch f l h
   | h < l     = l
   | v == GT   = binSearch f l (mid-1)
